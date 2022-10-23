bbcb0457b9db413c931aecc157d3eceab313cf53
Victor <victor.liracarlos@gmail.com>
2022-10-23 10:47:47 -0300

    Fix erro storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/addon-a11y',
+    '@storybook/addon-a11y'
   ],
   "framework": "@storybook/react",
   "core": {
@@ -16,11 +16,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-  viteFinal: (config, {configType} ) => {
-		if (configType === 'PRODUCTION') {
-			config.base = '/nome-do-repo/'
-		}
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-lab-design-system/'
+    }
 
-		return config
-	},
+    return config
+  }
 }
